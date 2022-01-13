f7948138820a05b635196956467a3037cfd76ddc
Timothy Jones <tim@zmthy.net>
2022-01-14 09:38:33 +1300

    Remove default config preamble

--- custom.el
+++ custom.el
@@ -5,14 +5,17 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "246a9596178bb806c5f41e5b571546bb6e0f4bd41a9da0df5dfbca7ec6e2250c" default))
+ '(display-line-numbers-type 'relative)
  '(evil-move-beyond-eol nil)
  '(evil-shift-width 2)
  '(fill-column 80)
  '(magit-branch-adjust-remote-upstream-alist '(("develop" "develop")))
  '(magit-clone-default-directory "~/Code/")
  '(magit-clone-set-remote\.pushDefault t)
+ '(org-directory "~/Documents/")
  '(sentence-end-double-space nil)
  '(standard-indent 2)
+ '(user-mail-address "tim@zmthy.net")
  '(whitespace-style '(face trailing tab tab-mark)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
