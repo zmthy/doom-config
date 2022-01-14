22ef0c9740e2fb802500448c92e5bc797c10c11a
Timothy Jones <tim@zmthy.net>
2022-01-14 16:37:11 +1300

    Do not prompt for confirmation when exiting Emacs

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(confirm-kill-emacs nil)
  '(custom-safe-themes
    '("835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "246a9596178bb806c5f41e5b571546bb6e0f4bd41a9da0df5dfbca7ec6e2250c" default))
  '(display-line-numbers-type 'relative)
