3f101a670d5b5930d35bc2b1d22ae6b1ea32f20d
Timothy Jones <tim@zmthy.net>
2022-01-25 13:45:35 +1300

    Prefer the pyright LSP server for Python

--- init.el
+++ init.el
@@ -154,9 +154,10 @@
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       (python +conda    ; beautiful is better than ugly
+       (python +conda
                +pyenv
-               +lsp)
+               +lsp
+               +pyright) ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
